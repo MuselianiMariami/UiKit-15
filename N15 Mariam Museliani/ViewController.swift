c1d831431a297c18b9984c73e498f15c3e8a2266
MuselianiMariami <museliani61@gmail.com>
2024-04-22 14:00:55 +0400

    Initial Commit

---
+++ N15 Mariam Museliani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  N15 Mariam Museliani
+//
+//  Created by Mariam Museliani on 22.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
